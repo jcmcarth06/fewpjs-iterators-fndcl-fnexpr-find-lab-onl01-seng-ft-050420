b3e0c8a054d969e2ec3ee7d070a91b41ae6d0aee
Joshua McArthur <jcmcarth06@gmail.com>
2020-08-17 21:31:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin = Array => {
+  let winner = Array.find( game => game.result === "W")
+  if(winner) = {
+    return winner.year
+  } else 
+  return undefined
+}
